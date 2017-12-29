a022702482ebfc97a42de55ac25f45fa172cea7e
onahump <oswaldo@makingdevs.com>
2017-12-29 15:21:50 -0600

    Comentarios multilinea

--- methods.rb
+++ methods.rb
@@ -2,3 +2,11 @@ puts "Oswaldo".length    #dice el tamaño
 puts "Oswaldo".reverse	 #Cambia el orden alreves
 puts "Oswaldo".upcase    #Cambia las letras a mayusculas
 puts "Oswaldo".downcase  #cambia las letras a minusculas
+
+
+=begin    Comentarios multilinea
+puts "Oswaldo".length    
+puts "Oswaldo".reverse	 
+puts "Oswaldo".upcase   
+puts "Oswaldo".downcase  
+=end
