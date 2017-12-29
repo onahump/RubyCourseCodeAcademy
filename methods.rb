ed0a75d9eda46ff59dda86f595d61bf5a6af9224
onahump <oswaldo@makingdevs.com>
2017-12-29 15:19:40 -0600

    metodo upercase & downcase

--- methods.rb
+++ methods.rb
@@ -1,2 +1,4 @@
 puts "Oswaldo".length    #dice el tamaño 
 puts "Oswaldo".reverse	 #Cambia el orden alreves
+puts "Oswaldo".upcase    #Cambia las letras a mayusculas
+puts "Oswaldo".downcase  #cambia las letras a minusculas
