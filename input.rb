c065886a3a6e9f9c112c88a9e8fa5e166a69f8c2
onahump <oswaldo@makingdevs.com>
2017-12-29 23:51:29 -0600

    string interpolation

--- input.rb
+++ input.rb
@@ -6,3 +6,9 @@ print "where do you live?"
 city = gets.chomp
 print "Where state do you live?"
 state = gets.chomp
+
+
+puts "your name's #{first_name}"
+puts "your last name's #{last_name}"
+puts "your are from #{city}"
+puts "your live in  #{state}"
