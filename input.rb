e09d9c41158b8914e7e14f09dd9fd5dc84619570
onahump <oswaldo@makingdevs.com>
2017-12-29 23:47:17 -0600

    Agregando nuevos ejemplos

--- input.rb
+++ input.rb
@@ -1,2 +1,8 @@
 print "What's your first name?"
 first_name = gets.chomp
+print "What's your last name?"
+last_name = gets.chomp
+print "where do you live?"
+city = gets.chomp
+print "Where state do you live?"
+state = gets.chomp
