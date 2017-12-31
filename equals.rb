4173327435fcf4a72bda61dbf72bcd606a83e866
onahump <oswaldo@makingdevs.com>
2017-12-30 22:08:13 -0600

    Agregando mas ejemplos a equals.rb

--- equals.rb
+++ equals.rb
@@ -9,3 +9,15 @@ test_2 = 21 < 30 #Menor a
 test_3 = 9 >= 9	 #Mayor o igual 
 
 test_4 = -11 <=  4	#menor o igual
+
+# test_5 = 77 != 77
+test_5 = false
+
+# test_6 = -4 <= -4
+test_6 = true
+
+# test_7 = -44 < -33
+test_7 = true
+
+# test_8 = 100 == 1000
+test_8 = false
