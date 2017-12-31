f8e2be65d3938c9549172efc85bb52f9dd39541a
onahump <oswaldo@makingdevs.com>
2017-12-30 21:20:39 -0600

    Modificando el programa con capitalize

--- input.rb
+++ input.rb
@@ -1,12 +1,15 @@
 print "What's your first name?"
 first_name = gets.chomp
+first_name.capitalize!
 print "What's your last name?"
 last_name = gets.chomp
+last_name.capitalize!
 print "where do you live?"
 city = gets.chomp
+city.capitalize!
 print "Where state do you live?"
 state = gets.chomp
-
+state.upcase!
 
 puts "your name's #{first_name}"
 puts "your last name's #{last_name}"
