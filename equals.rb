413f93e75038d489ec7fef2e9c9b177307edb5d8
onahump <oswaldo@makingdevs.com>
2017-12-30 22:55:37 -0600

    agregando mas ejemplos

--- equals.rb
+++ equals.rb
@@ -21,3 +21,12 @@ test_7 = true
 
 # test_8 = 100 == 1000
 test_8 = false
+
+# boolean_1 = 77 < 78 && 77 < 77
+boolean_1 = false
+
+# boolean_2 = true && 100 >= 100
+boolean_2 = true
+
+# boolean_3 = 2**3 == 8 && 3**2 == 9
+boolean_3 = true
