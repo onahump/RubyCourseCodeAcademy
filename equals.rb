5f36f666418c58b609db67cd13f0a7fa59a1ea79
onahump <oswaldo@makingdevs.com>
2017-12-30 21:52:33 -0600

    Agregando mas diferencias e equivalentes

--- equals.rb
+++ equals.rb
@@ -1,3 +1,11 @@
 is_true = 2 != 3 #Es diferente de 3
 
 is_false = 2 == 3 #Es igual a 3
+
+test_1 = 17 > 16 #Mayor a
+
+test_2 = 21 < 30 #Menor a
+
+test_3 = 9 >= 9	 #Mayor o igual 
+
+test_4 = -11 <=  4	#menor o igual
